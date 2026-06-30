b26dab7c57ef177b0f54498e2de5a1bced4ca3f3
maniac1217 <maniac1217@gmail.com>
2026-06-30 19:35:46 +0900

    Refactor script.js to improve structure and readability

--- script.js
+++ script.js
@@ -102,9 +102,9 @@ async function sendToNotion(reservationData) {
     };
 
     // ⚠️ 중요: 방금 1단계에서 만든 본인의 Cloudflare Workers 주소를 여기에 붙여넣으세요!
-    const NOTION_API_KEY = "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";
+    //const NOTION_API_KEY = "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";
     //const myWorkerUrl = "https://damp-truth-0bc8.maniac1217.workers.dev/"; 
-
+      const MY_WORKER_URL = "https://hook.eu1.make.com/ubacqhwvsv9szj8prbu1w1aiqjp99rvr";
     try {
         console.log("노션 데이터 전송 시작...");
         
